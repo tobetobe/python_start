a01ae38cd2868b326aea1438f9ad2a00ef4d7065
tobetobe <949043032@qq.com>
2019-10-24 01:43:25 +0800

    今日份的学习

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # python_start
-python学习collection
+
+python 学习 collection
+
+## list operation
+
+list = []
+
+- list.append( value )
+- del list[ index ]
+- list.pop( index )
+  - return the item
+- list.remove( value )
+  - only del first item
+- list.sort()
+- sorted(list)
+  - 临时排序
+- list.reverse()
+- 列表解析
---
+++ hello.py
@@ -0,0 +1,21 @@
+# for index, item in enumerate(['a', 'b', 'c']):
+#     print(index, item)
+
+
+def printHello():
+    """ say Hello to the console
+
+    Just print a "hello
+    """
+    print("hello")
+
+
+# printHello()
+
+# list = ['bca', 'abc', 'ccc']
+# sorted(list)
+# print(list)
+
+# list = [v*2 for v in range(1, 10)]
+
+# print(list)
